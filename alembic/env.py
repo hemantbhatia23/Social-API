cced6ac6c7f1c439c947a687f77b1a14df6f4741
Hemant Bhatia <59552830+hemantbhatia23@users.noreply.github.com>
2022-08-07 16:05:21 +0530

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -12,7 +12,7 @@ from app.config import settings
 config = context.config
 config.set_main_option(
     "sqlalchemy.url", f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-    #'postgresql+psycopg2://postgres:postgresql23@localhost:5432/fastapi'
+    
 )
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
